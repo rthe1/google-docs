4a0f6ec3db1f1662244c097abbeb11338957535d
Reshawn Hamilton <reshawnhamilton97@gmail.com>
2022-11-24 17:59:55 -0500

    routes and router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import TextEditor from './components/TextEditor'
+import {
+  BrowserRouter,
+  Switch,
+  Redirect,
+  Route,
+  BrowserRouter
+} from 'react-router-dom';
+import {v4 as uuid} from 'uuid';
 
 function App() {
   return (
-    <div>
-      <TextEditor />
-    </div>
-   
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to={`/documents/${uuid()}`}/>
+        </Route>
+        <Route path="/documents/:id" exact>
+          <Redirect/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
