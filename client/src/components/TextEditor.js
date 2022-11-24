7fe8ef92988a690fe25a90d33deada65deb7e5da
Reshawn Hamilton <reshawnhamilton97@gmail.com>
2022-11-24 01:09:56 -0500

    typo fixed

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -3,8 +3,7 @@ import 'quill/dist/quill.snow.css';
 import Quill from 'quill';
 import io from 'socket.io-client';
 
-const [socket, setSocket] = useState()
-const [quill, setQuill] = useState()
+
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -20,6 +19,9 @@ const TOOLBAR_OPTIONS = [
 
 export default function TextEditor() {
 
+  const [socket, setSocket] = useState()
+const [quill, setQuill] = useState()
+
   useEffect(() => {
     //Sets socket state
     const s = io('http://localhost:3001');
@@ -30,6 +32,50 @@ export default function TextEditor() {
       s.disconnect()
     }
   }, [])
+
+
+  useEffect(() => {
+
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") return
+      socket.emit('send-changes', delta)
+      console.log(delta)
+    }
+    // check if quill and socket have been set yet
+    if (quill == null || socket == null) return
+
+    //add event handler
+    quill.on('text-change', handler)
+    
+
+    return () => {
+    // removes event handler
+    quill.off('text-change', handler)
+    }
+  },[socket, quill])
+
+
+
+  useEffect(() => {
+    // check if quill and socket have been set yet
+    if (quill == null || socket == null) return
+
+    const handler = (delta) => {
+    quill.updateContents(delta)
+    console.log(delta)
+    }
+
+    //add event handler
+    socket.on('recieve-changes', handler)
+    console.log('recieve changes')
+
+    return () => {
+    // removes event handler
+    socket.off('recieve-changes', handler)
+    }
+  },[socket, quill])
+
+
 
   const wrapperRef = useCallback((wrapper) => {
     // incase .container isnt loaded yet
--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-const { Socket } = require('socket.io');
-
 const io = require('socket.io')(3001, {
   cors: {
     origin: 'http://localhost:3000',
